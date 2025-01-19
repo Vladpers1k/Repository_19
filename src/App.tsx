5ddb896a531d9924db11b852705e5e7a61f1b40f
Vladpers1k <vladik340123@gmail.com>
2025-01-19 13:17:37 +0200

    changed body theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from 'store/store'
 import routes from './routes'
@@ -7,7 +7,13 @@ import './App.css'
 const App: React.FC = () => {
   const theme = useSelector((state: RootState) => state.settings.theme)
 
-  document.body.className = theme
+  useEffect(() => {
+    document.body.className = theme
+
+    return () => {
+      document.body.className = ''
+    }
+  }, [theme])
 
   return (
     <div>
