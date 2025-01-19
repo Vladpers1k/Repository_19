d52c69c4e7611dc4b1884091e38161b4bc3be965
Vladpers1k <vladik340123@gmail.com>
2025-01-19 13:15:07 +0200

    big change in settings.tsx

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState, AppDispatch } from 'store/store'
-import { toggleTheme } from '../store/settingsSlice'
+import { toggleThemeAsync } from '../store/settingsSlice'
 
 const Settings: React.FC = () => {
   const dispatch: AppDispatch = useDispatch()
-  const theme = useSelector((state: RootState) => state.settings.theme)
+  const { theme, status } = useSelector((state: RootState) => state.settings)
 
   return (
     <div>
       <h2>Settings Page</h2>
       <p>Current Theme: {theme}</p>
-      <button onClick={() => dispatch(toggleTheme())}>Toggle Theme</button>
+      <p>Status: {status}</p>
+      <button onClick={() => dispatch(toggleThemeAsync())} disabled={status === 'loading'}>
+        {status === 'loading' ? 'Changing Theme...' : 'Toggle Theme'}
+      </button>
     </div>
   )
 }
